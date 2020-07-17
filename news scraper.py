71d7a4eb9f81940be9f0435b0394764dd7090fc3
daniellynch98 <32166604+daniellynch98@users.noreply.github.com>
2020-07-17 17:25:01 +0100

    Add files via upload

---
+++ news scraper.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[111]:
+
+
+import  requests, os, bs4, openpyxl, sys
+os.chdir('/Users/daniellynch/Downloads/News Bias Files/Right')
+
+url = "https://mediabiasfactcheck.com/right/"
+res = requests.get(url)
+res.raise_for_status()
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+table = soup.select_one('#mbfc-table')
+get_links = table.find_all('a')
+x = 0
+for link in get_links:
+    link = link.get("href")
+    try:
+        res = requests.get(link)
+        if res.status_code == 404:
+            print('404 for: ' + link)
+        else:
+            link_soup = bs4.BeautifulSoup(res.text, 'html.parser')
+            print('Here is a link: ' + link)
+            try:
+                Biased = link_soup.find_all('p')
+                title = link_soup.select_one('#mh-mobile > div.container.mh-mobile > div.wrapper.clearfix')
+                organisation = title.find('h1')
+                organisation = organisation.text
+                #stops python thinking title is a directory
+                if '/' in organisation:
+                    print (organisation)
+                    organisation = organisation.replace('/', " ")
+            except AttributeError:
+                print('dead link')
+                continue
+        for i in Biased:
+            if 'Factual Reporting:'  in i.text:
+                new_file = open((organisation) +'.txt', 'w')
+                new_file.write(organisation)
+                new_file.write('\n' + i.text)
+                new_file.close()
+                continue
+            
+    except requests.exceptions.MissingSchema:
+        continue
+print('Finished')
+        
+
+
+# In[112]:
+
+
+file_name_list = []
+for foldername, subfolders, filenames in os.walk('/Users/daniellynch/Downloads/News Bias Files/Right'):
+    for file in filenames:
+        if file.endswith('.txt'):
+            mini_list = []
+            f = open(file, 'r')
+            read_text = f.readlines()
+            file_name_list.append(read_text)
+            
+sorted_list = sorted(file_name_list)
+
+        
+wb = openpyxl.load_workbook('/Users/daniellynch/Downloads/News Bias Files/Media Bias Fact Check.xlsx')
+
+sheet = wb['Right']
+
+for content in sorted_list:
+    sheet.append(content)
+    
+wb.save('/Users/daniellynch/Downloads/News Bias Files/Media Bias Fact Check.xlsx')
+wb.close()
+print('Finished')
+
+        
+
+
+# In[18]:
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
